ba22ad13fa51fda2c481c97b630a47ce108e53d8
Joda23 <tojdk@gmx.de>
2020-06-09 19:33:30 +0200

    Christinas Blog, first commit

---
+++ virt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\documents\jona\_uni\_aktuell\systementwicklung\christina\virt\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
