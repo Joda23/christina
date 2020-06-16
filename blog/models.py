bad09a44625884f8c4a1271d9bba3a5610c858f7
Joda23 <tojdk@gmx.de>
2020-06-16 13:11:37 +0200

    distance_run als neue Eigenschaft des Post

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    distance_run = models.DecimalField(max_digits=4, decimal_places=2, default=[0,00])
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
