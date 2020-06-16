36343f21805e27b4fd9575590ca6ef5fc700de92
Joda23 <tojdk@gmx.de>
2020-06-16 15:20:49 +0200

    Drop-Down fuer performance_satisfaction hinzugefuegt

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,23 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    LEVEL_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     distance_run = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
-    performance_satisfaction = models.IntegerField([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    performance_satisfaction = models.IntegerField(choices=LEVEL_CHOICES, default=1)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
